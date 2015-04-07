be7263b5805d1004e448eab02d5ddb8d1ce53abf
tormaviktor <torma.viktor@gmail.com>
2015-04-07 19:06:48 +0200

    #35

--- festivalplum-android/FestivalPlum/src/hu/festivalplum/utils/SideBar.java
+++ festivalplum-android/FestivalPlum/src/hu/festivalplum/utils/SideBar.java
@@ -7,37 +7,53 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SectionIndexer;
 
 public class SideBar extends View {
     private char[] l;
     private SectionIndexer sectionIndexter = null;
     private ListView list;
-    private final int m_nItemHeight = 29;
+    private int m_nItemHeight = 29;
+    private int textSize = 20;
     public SideBar(Context context) {
         super(context);
         init();
+        calculate();
     }
     public SideBar(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
+        calculate();
     }
     private void init() {
         //l = new char[] { 'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z' };
         l = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
         setBackgroundColor(0x44FFFFFF);
     }
+
+    private void calculate(){
+        DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+        Double h = metrics.heightPixels * 0.7 / l.length;
+        m_nItemHeight = h.intValue();
+        textSize = m_nItemHeight - 10;
+    }
+
     public SideBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init();
+        calculate();
     }
+
     public void setListView(ListView _list) {
         list = _list;
         sectionIndexter = (SectionIndexer) _list.getAdapter();
     }
+
     public boolean onTouchEvent(MotionEvent event) {
         super.onTouchEvent(event);
         int i = (int) event.getY();
@@ -59,10 +75,11 @@ public class SideBar extends View {
         }
         return true;
     }
+
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(0xFFA6A9AA);
-        paint.setTextSize(20);
+        paint.setTextSize(textSize);
         paint.setTextAlign(Paint.Align.CENTER);
         float widthCenter = getMeasuredWidth() / 2;
         for (int i = 0; i < l.length; i++) {
